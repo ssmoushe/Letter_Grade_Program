9693ecb27b841413e0f2687fa5383b8f8eb6aa89
Mahir Shahriar <106269171+ssmoushe@users.noreply.github.com>
2024-02-03 21:57:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var mark = prompt("Enter input exam marks: ")
+
+if(mark>=80)
+console.log("A+");
+
+
+else if (mark>=70)
+console.log("A");
+
+else if (mark>=65)
+console.log("A-");
+
+else if (mark>=60)
+console.log("B+");
+
+else if (mark>=55)
+console.log("B-");
+
+else if (mark>=50)
+console.log("C+");
+
+else if (mark>=45)
+console.log("C-");
+
+else if (mark>=40)
+console.log("D");
+
+
+
+else
+console.log("Fail");
