1d62c7f6118ac0c562791b7345284df3c7947f06
Mahir Shahriar <106269171+ssmoushe@users.noreply.github.com>
2024-02-04 11:38:56 +0600

    4th

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 var mark = prompt("Enter input exam marks: ")
 
-if(mark>=80)
+if  (mark > 100 || mark < 0)
+console.log("Invalid marks");
+
+else if (mark>=80 && mark<=100)
 console.log("A+");
 
 
-else if (mark>=70)
+else if (mark>=70 && mark<=79)
 console.log("A");
 
-else if (mark>=65)
+else if (mark>=65 && mark<=69)
 console.log("A-");
 
-else if (mark>=60)
+else if (mark>=60 && mark<=64)
 console.log("B+");
 
-else if (mark>=55)
+else if (mark>=55 && mark<=59)
 console.log("B-");
 
-else if (mark>=50)
+else if (mark>=50 && mark<=54)
 console.log("C+");
 
-else if (mark>=45)
+else if (mark>=45 && mark<=49)
 console.log("C-");
 
-else if (mark>=40)
+else if (mark>=40 && mark<=44)
 console.log("D");
 
 
